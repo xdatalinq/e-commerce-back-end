5ff5f1098a350cec74504f2f3750ac6af460768a
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-08-10 12:59:14 -0400

    Added validators and reference found syntax in sequelize manual: https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -23,7 +24,7 @@ Product.init(
       type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
-
+        isDecimal: true
       }
     },
     stock: {
@@ -31,11 +32,15 @@ Product.init(
       allowNull: false,
       defaultValue: 10,
       validate: {
-
+        isNumeric: true
       }
     },
     category_id: {
       type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: 'id'
+      }
     }  
   },
   {

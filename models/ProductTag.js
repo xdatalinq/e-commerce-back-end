8f3a8c91f5a4cd78326c10bf7d94c382e0765ead
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-08-10 18:37:45 -0400

    un-comment ProductTag. Found issue in index.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -13,20 +13,20 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true
     },
-    // product_id: {
-    //   type: DataTypes.INTEGER,
-    //   references: {
-    //     model: Product,
-    //     key: 'id'
-    //   }
-    // },
-    // tag_id: {
-    //   type: DataTypes.INTEGER,
-    //   references: {
-    //     model: Tag,
-    //     key: 'id'
-    //   }
-    // }  
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag,
+        key: 'id'
+      }
+    }  
   },
   {
     sequelize,

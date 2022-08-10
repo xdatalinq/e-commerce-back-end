ebfde097ad3344d02c1b8368c558726e790319f9
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-08-10 12:45:17 -0400

    Product model according to instructions, I need to check sequelize manual for syntax of validate and how to reference another model for category_id still

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,34 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+    }  
   },
   {
     sequelize,
